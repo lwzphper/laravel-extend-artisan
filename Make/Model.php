aec35751afb139bc03c62b1fcf66a960af6c86c7
zhen <1149243351@qq.com>
2021-11-29 16:11:16 +0800

    调整创建模型方法

--- Make/Model.php
+++ Make/Model.php
@@ -3,6 +3,7 @@
 namespace Lwz\LaravelExtend\Artisan\Make;
 
 use Illuminate\Foundation\Console\ModelMakeCommand as Command;
+use Illuminate\Support\Str;
 use Symfony\Component\Console\Input\InputArgument;
 
 class Model extends Command
@@ -31,5 +32,54 @@ class Model extends Command
     protected function getCreateModelName(): string
     {
         return 'Models';
+    }
+
+
+    /**
+     * Create a model factory for the model.
+     *
+     * @return void
+     */
+    protected function createFactory()
+    {
+        $factory = Str::studly($this->argument('name'));
+
+        $this->call('ext-make:factory', [
+            'name' => "{$factory}Factory",
+            '--model' => $this->qualifyClass($this->getNameInput()),
+        ]);
+    }
+
+    /**
+     * Create a migration file for the model.
+     *
+     * @return void
+     */
+    protected function createMigration()
+    {
+        $table = Str::snake(Str::pluralStudly(class_basename($this->argument('name'))));
+
+        if ($this->option('pivot')) {
+            $table = Str::singular($table);
+        }
+
+        $this->call('ext-make:migration', [
+            'name' => "create_{$table}_table",
+            '--create' => $table,
+        ]);
+    }
+
+    /**
+     * Create a seeder file for the model.
+     *
+     * @return void
+     */
+    protected function createSeeder()
+    {
+        $seeder = Str::studly(class_basename($this->argument('name')));
+
+        $this->call('ext-make:seeder', [
+            'name' => "{$seeder}Seeder",
+        ]);
     }
 }
