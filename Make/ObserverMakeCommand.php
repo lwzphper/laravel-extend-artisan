6d4091516fe1bbce88a39befc871ecc065167322
zhen <1149243551@qq.com>
2021-12-05 14:27:52 +0800

    新增观察者

--- Make/ObserverMakeCommand.php
+++ Make/ObserverMakeCommand.php
@@ -2,9 +2,7 @@
 
 namespace Lwz\LaravelExtend\Artisan\Make;
 
-use InvalidArgumentException;
 use Symfony\Component\Console\Input\InputArgument;
-use Symfony\Component\Console\Input\InputOption;
 use Illuminate\Foundation\Console\ObserverMakeCommand as Command;
 
 class ObserverMakeCommand extends Command
@@ -21,6 +19,34 @@ class ObserverMakeCommand extends Command
     public function configure()
     {
         $this->addArgument('service', InputArgument::REQUIRED, 'service name');
+    }
+
+    /**
+     * Replace the model for the given stub.
+     *
+     * @param  string  $stub
+     * @param  string  $model
+     * @return string
+     */
+    protected function replaceModel($stub, $model)
+    {
+        $modelClass = $this->parseModel($model);
+
+        $replace = [
+            'DummyFullModelClass' => $modelClass,
+            '{{ namespacedModel }}' => $modelClass,
+            '{{namespacedModel}}' => 'Models\\' . $modelClass,
+            'DummyModelClass' => class_basename($modelClass),
+            '{{ model }}' => class_basename($modelClass),
+            '{{model}}' => class_basename($modelClass),
+            'DummyModelVariable' => lcfirst(class_basename($modelClass)),
+            '{{ modelVariable }}' => lcfirst(class_basename($modelClass)),
+            '{{modelVariable}}' => lcfirst(class_basename($modelClass)),
+        ];
+
+        return str_replace(
+            array_keys($replace), array_values($replace), $stub
+        );
     }
 
     /**
